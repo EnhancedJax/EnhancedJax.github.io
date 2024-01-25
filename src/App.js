198ceaea4752be1497b0d39aeba1f99b7831d59e
Jax <enhancedjax@gmail.com>
2024-01-25 10:21:11 +0800

    TailwindCSS

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import Home from './pages/Home';
 import Navbar from "./components/Navbar";
 import Notes from "./pages/Notes";
 // import './App.css';
+import styles from "./index.css"
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/hkunotes" element={<Notes />} />
-      </Routes>
+    <>w
+      <p class="bg-red-50">HELLO WORLD</p>
+      <p class="text-center text-9xl">HELLO WORLD</p>
+      <p class="text-center text-9xl">HELLO WORLD</p>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class
+  mode: 'jit',
+  // purge: ['./src/**/*.{js}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
+    fontFamily: {
+      sans: ['"cabinet-grotesk"', 'sans-serif']
+    }
   },
   plugins: [],
 }
 
+
+
+// TODO: Custom font, colors and jit setup
