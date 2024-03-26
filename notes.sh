6a49477063da43500f8f6f064c1a5a6b7bd95404
Jax <enhancedjax@gmail.com>
2024-03-26 19:42:05 +0800

    EOL

--- notes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cwd=$(pwd)
-find ~/Documents/Study-Materials -type f -name "*Notes.pdf" | 
-    while IFS= read -r  file; do
-        echo "Copying $file..."
-        newname=$(basename "$file" | sed 's/ Notes//g')
-        cp -f "$file" "$cwd/public/notes/$newname" 
-    done
